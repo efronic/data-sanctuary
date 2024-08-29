0ae2e3da7f2c012e0f30b8c04cb283aadfa25e3b
Efron <eahmadifar@outlook.com>
2024-08-29 16:47:22 +1000

    Refactor page component to use Convex API for document creation and retrieval

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 'use client';
 
+import { api } from '@/convex/_generated/api';
+import { createDocument } from '@/convex/documents';
 import { SignInButton, UserButton } from '@clerk/nextjs';
-import { Authenticated, Unauthenticated } from 'convex/react';
+import { Authenticated, Unauthenticated, useMutation, useQuery } from 'convex/react';
 
 export default function Home() {
+  const createDocument = useMutation(api.documents.createDocument);
+  const documents = useQuery(api.documents.getDocuments);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Unauthenticated>
@@ -11,6 +16,12 @@ export default function Home() {
       </Unauthenticated>
       <Authenticated>
         <UserButton />
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => { createDocument({ title: 'Hello World!' }) }}> Click Me </button>
+        <ul>
+          {documents?.map((document) => (
+            <li key={document._id}>{document.title}</li>
+          ))}
+        </ul>
       </Authenticated>
     </main>
   );
